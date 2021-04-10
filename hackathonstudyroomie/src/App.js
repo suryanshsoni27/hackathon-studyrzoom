767daa8c4a9ba6285c436aa56062b588c1f917aa
suryanshsoni27 <szscode2727@gmail.com>
2021-04-10 16:04:20 -0500

    Work

--- hackathonstudyroomie/src/App.js
+++ hackathonstudyroomie/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from "/components/header"
+import List1 from "/components/List1"
 
 function App() {
   return (
     <div className="App">
+      {/*header*/}
+      <Header></Header>
+      {/*list1*/}
+      {/*list2*/}
     </div>
   );
 }
